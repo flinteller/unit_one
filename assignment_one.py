c2e425290b0f1f9d106226b508ed7704b332700a
Flint <flint@iMac1.local>
2018-09-14 14:00:38 -0400

    Final version face

--- assignment_one.py
+++ assignment_one.py
@@ -1,3 +1,9 @@
+# Flint Eller
+# 9/13/18
+# Draws a face
+
+
+
 import turtle
 
 turtle.speed(11)
@@ -11,7 +17,7 @@ turtle.pendown()
 turtle.color("#FEDC5F")
 
 turtle.begin_fill()
-
+# This draws a yellow face
 turtle.circle(200)
 
 turtle.end_fill()
@@ -21,6 +27,7 @@ turtle.penup()
 turtle.goto(-90,150)
 
 
+# This creates a function so I can just call it instead of drawing a whole new eye
 def draw_eye():
     turtle.color("#000000")
     turtle.begin_fill()
@@ -40,19 +47,17 @@ draw_eye()
 
 turtle.penup()
 
-turtle.goto(-15,125)
+turtle.goto(-20,125)
 
 
-def draw_nose():
-    turtle.color("#000000")
-    turtle.begin_fill()
-    for x in range(3):
-        turtle.forward(40)
-        turtle.right(120)
-    turtle.end_fill()
+# This draws a balck tringle for the nose
+turtle.color("#000000")
+turtle.begin_fill()
+for w in range(3):
+    turtle.forward(50)
+    turtle.right(120)
+turtle.end_fill()
 
-
-draw_nose()
 
 turtle.penup()
 
@@ -63,15 +68,54 @@ turtle.pendown()
 turtle.pencolor("#000000")
 
 turtle.width(15)
-
+# This draw a mouth
 turtle.forward(100)
 
 turtle.width(1)
 
 turtle.penup()
 
-turtle.goto(-110,330)
+turtle.goto(-55,330)
 
+# This draws a series of brown triangles for the hair
+turtle.color("#9C6503")
+turtle.begin_fill()
+for x in range(5):
+    for y in range(3):
+        turtle.right(120)
+        turtle.forward(40)
+    turtle.penup()
+    turtle.forward(40)
+    turtle.pendown()
+turtle.end_fill()
+
+
+turtle.penup()
+
+turtle.goto(-90,320)
+
+turtle.pendown()
+
+
+# This makes a single brown triangle to add next to the hair
+def draw_one_hair():
+    turtle.color("#9C6503")
+    turtle.begin_fill()
+    for z in range(3):
+        turtle.right(120)
+        turtle.forward(40)
+    turtle.end_fill()
+
+
+draw_one_hair()
+
+turtle.penup()
+
+turtle.goto(140,320)
+
+turtle.pendown()
+
+draw_one_hair()
 
 
 turtle.exitonclick()
